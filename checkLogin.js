d8c943bf6eff17afde73d2e75c46ed04d0b63152
Troy Purvis <tpurvis1996@gmail.com>
2019-04-17 23:41:14 -0400

    Modularized login check for nav bar for each page by putting script in separate file (checkLogin.js)

---
+++ checkLogin.js
@@ -0,0 +1,10 @@
+
+var x = document.cookie;
+var ca = x.split("; ");
+
+for(var i = 0; i < ca.length; i++){
+    var c = ca[i].split("=");
+    if(c[0] == "firstname"){
+    document.getElementById("loginLink").innerHTML= '<a href="profile.html" </a> Profile';
+    }
+}

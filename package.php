74cb3f3b38d65d7e322337061b99b6f9941b6048
Jenna Lovett <jennablair12@gmail.com>
2019-04-20 15:33:45 -0400

    updated pdo connection

--- package.php
+++ package.php
@@ -6,7 +6,7 @@
     //echo include('http://localhost/Project/database_connection.php');
     //$connect = new PDO('mysql:host=localhost;dbname=BlackMesaTravel', 'iw3htp', 'password');
     //connecting to AWS instance of database
-    $connect = new PDO('mysql:host=rds-mysql-10mintutorial.cj3sjwqrps9d.us-east-1.rds.amazonaws.com;port=3306;dbname=BlackMesaTravel', 'masterUsername', 'blackmesatravel');
+    $connect = new PDO('mysql:host=mysql-bmtravel.cj3sjwqrps9d.us-east-1.rds.amazonaws.com;port=3306;dbname=BlackMesaTravel', 'masterUsername', 'blackmesatravel');
     //echo $connect;
 ?>
 <!DOCTYPE html>

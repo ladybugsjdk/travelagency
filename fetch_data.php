b0cb5c0a2ef23070db0cb60e5a2631dc9aded7c8
Jenna Lovett <jennablair12@gmail.com>
2019-04-20 15:33:12 -0400

    updated pdo connection

--- fetch_data.php
+++ fetch_data.php
@@ -6,7 +6,7 @@
  */
 
 //connection created
-$connect = new PDO('mysql:host=rds-mysql-10mintutorial.cj3sjwqrps9d.us-east-1.rds.amazonaws.com;port=3306;dbname=BlackMesaTravel', 'masterUsername', 'blackmesatravel');
+$connect = new PDO('mysql:host=mysql-bmtravel.cj3sjwqrps9d.us-east-1.rds.amazonaws.com;port=3306;dbname=BlackMesaTravel', 'masterUsername', 'blackmesatravel');
 
 //setting base query
 if(isset($_POST["action"])) {

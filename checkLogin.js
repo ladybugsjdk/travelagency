424f2d184df4cc4cacc15bd5477ee399c3af2142
Houston Littlejohn <43423116+ThatColossalWreck@users.noreply.github.com>
2019-04-27 14:49:30 -0400

    Changed the routing to profile.php instead of the html

--- checkLogin.js
+++ checkLogin.js
@@ -1,10 +1,9 @@
-
 var x = document.cookie;
 var ca = x.split("; ");
 
 for(var i = 0; i < ca.length; i++){
     var c = ca[i].split("=");
     if(c[0] == "firstname"){
-    document.getElementById("loginLink").innerHTML= '<a href="profile.html" </a> Profile';
+    document.getElementById("loginLink").innerHTML= '<a href="profile.php" </a> Profile';
     }
 }

25c020b5ab10fe56b83d0929829d0c843a3bbdbb
Troy Purvis <tpurvis1996@gmail.com>
2019-04-28 00:35:56 -0400

    check for login on package page

--- package.php
+++ package.php
@@ -140,7 +140,7 @@
       <!-- Navigation Bar -->
       <ul class="navbar">
          <li id="navbranding" class="navitem"><a href="index.html">Black Mesa Travel</a></li>
-         <li class="navitem"><a href="login.html">Login/Signup</a></li>
+         <li class="navitem" id="loginLink"><a href="login.html">Login/Signup</a></li>
          <li class="navitem"><a href="package.php">Premium Packages</a></li>
          <li class="navitem"><a href="#">Trending</a></li>
          <li class="navitem"><a href="index.html">Home</a></li>
@@ -357,6 +357,8 @@
             
         });
     </script>
+
+    <script src="checkLogin.js"></script>
    </body>
    
 </html>
